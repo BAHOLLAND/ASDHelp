6b487de074cf69f87c154e32465f9b9c59a57683
Bronson Holland <baholland@student.fullsail.edu>
2020-05-29 09:51:01 -0700

    initial commit

---
+++ BronsonHolland_CE05/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace BronsonHolland_CE05
+{
+    public class Animal
+    {
+        int foodConsumed;
+        string treat;
+
+        public Animal()
+        {
+        }
+
+        public string Species
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ BronsonHolland_CE05/ITrainable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace BronsonHolland_CE05
+{
+    public Dictionary<string, string> Behaviors { get; set; }
+    public interface ITrainable
+    {
+    }
+    public string Perform(string signal)
+    {
+        throw new NullReferenceException();
+    }
+    public string Train(string signal, string behavior)
+    {
+        throw new NullReferenceException();
+    }
+}
